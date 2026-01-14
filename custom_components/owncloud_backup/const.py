a68f8fecf964928221e415b4d54864dd6a2b039a
René Bachmann <r.bachmann@bahmcloud.de>
2026-01-14 10:15:47 +0100

    Add constants for owncloud_backup component

---
+++ custom_components/owncloud_backup/const.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+DOMAIN = "owncloud_backup"
+
+CONF_BASE_URL = "base_url"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_BACKUP_PATH = "backup_path"
+CONF_VERIFY_SSL = "verify_ssl"
+
+DATA_CLIENT = "client"
+DATA_BACKUP_AGENT_LISTENERS = "backup_agent_listeners"
+
+TAR_PREFIX = "ha_backup_"
+TAR_SUFFIX = ".tar"
+META_SUFFIX = ".json"
